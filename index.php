eb0ab1bc35d622a2769434df0aa83f3eefb4a6a9
Javy018000 <81122338+Javy018000@users.noreply.github.com>
2022-10-20 09:56:03 -0500

    Cambios en index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">	<!--Idioma español-->
 	<link rel="icon" type="icon/png" href="https://vignette.wikia.nocookie.net/es.pokemon/images/2/2c/Pok%C3%A9mon_Jukebox_icono.png/revision/latest?cb=20150624132304">
 	<link rel="stylesheet" type="text/css" href="index.css">
-	<title>Mi Primera Pagina Web</title>
+	<title>Mi Primera Pagina Web en cloud</title>
 </head>
 <body>
 
@@ -13,13 +13,12 @@
 		<h1>Bienvenidos</h1>
 	</nav>
 
-	<h1 class="Titulo">¿Que es la Programacion web?</h1>
+	<h1 class="Titulo">Este es un ejemplo de despliegue para php en gcp</h1>
 
 	<div class="Conte">
 		<p>
-			La programación de los sitios web es una de las disciplinas dentro del mundo de Internet que más se ha desarrollado y no deja de sorprender día a día con las posibilidades que abre y genera, ya que no sólo consigue satisfacer necesidades que se generan, sino que sin la generación de necesidades ofrecen servicios a los usuarios que éstos no habían imaginado.<br><br>
-
-			En principio, el gran desarrollo de Internet se fundamentó en la posibilidad de enlazar a través de hipervículos diferentes páginas web lo que generó la enorme interconexión que es hoy Internet. Esta base que inició esta red, fué desarrollada y generó el lenguaje ícono de las páginas web que es HTML.
+		¿Qué es el despliegue en la nube? Un despliegue de Cloud Público (o Cloud multi-tenant) se caracteriza por ofrecer recursos
+		 TIC sobre infraestructuras compartidas entre múltiples clientes. A estos recursos el cliente accede a través de internet o mediante conexiones VPN.
 		</p>
 	</div>
 </body>
